d7f193cdf446a169e42ea98b4aef18e6dbc25e95
devNagy <devNagy24@outlook.com>
2024-01-23 18:05:08 -0800

    Initialized DadJoke API repository.

---
+++ src/main/java/org/example/dadjokeapi/DadJokeApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.dadjokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadJokeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadJokeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dadjokeapi/DadJokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dadjokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadJokeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
